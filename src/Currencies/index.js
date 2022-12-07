edd0d1ae3d9cade8d3e3340f3f44fab9f87b87b5
Alicja Jurczak <ala.jurczak@gmail.com>
2022-12-07 18:04:04 +0100

    currencies and select option added

---
+++ src/Currencies/index.js
@@ -0,0 +1,23 @@
+
+export const currencies = [
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.6739,
+    },
+    {
+        name: "Frank szwajcarski",
+        short: "CHF",
+        rate: 4.7371,
+    },
+    {
+        name: "Funt brytyjski",
+        short: "GBP",
+        rate: 5.4222,
+    },
+    {
+        name: "Dolar amerykański",
+        short: "USD",
+        rate: 4.7398,
+    },
+];
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,38 +1,40 @@
 import "./style.css";
+import { currencies } from "../Currencies";
 
 const Form = () => (
-    <form className="form">
-      <fieldset className="form__fieldset">
-        <legend className="form__legend">Kalkulator walut</legend>
-        <p>
-          <label>
-            <span className="form__labelText">
-              Kwota w zł: *
-            </span>
-            <input className="form__field" name="amount" placeholder="Kwota w zł" type="number" min="0" step="0.01" required autoFocus />
-          </label>
-        </p>
-        <p>
-          <label>
-            <span className="form__labelText">
-              Wybierz walutę: *
-            </span>
-            <select className="form__field" name="currencySelection">
-              <option value="EUR">Euro [EUR]</option>
-              <option value="CHF">Frank szwajcarski [CHF]</option>
-              <option value="GBP">Funt brytyjski [GBP]</option>
-              <option value="USD">Dolar amerykański [USD]</option>
-            </select>
-          </label>
-        </p>
-        <p className="form__result">Wynik przeliczenia: <strong>0</strong> </p>
-        <p>
-          <button className="form__button">Oblicz</button>
-        </p>
-        <p className="form__additionalInformation">*Pole obowiąznowe do wypełnienia</p>
-        <p className="form__additionalInformation">Obliczenia wykonano dla kursu walut z dnia 3.11.2022.</p>
-      </fieldset>
-    </form>
+  <form className="form">
+    <fieldset className="form__fieldset">
+      <legend className="form__legend">Kalkulator walut</legend>
+      <p>
+        <label>
+          <span className="form__labelText">
+            Kwota w zł: *
+          </span>
+          <input className="form__field" name="amount" placeholder="Kwota w zł" type="number" min="0" step="0.01" required autoFocus />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span className="form__labelText">
+            Wybierz walutę: *
+          </span>
+          <select className="form__field" name="currencySelection">
+            {currencies.map(currency => (
+              <option key={currency.short} value={currency.name}>
+                {currency.name}
+              </option>
+            ))};
+          </select>
+        </label>
+      </p>
+      <p className="form__result">Wynik przeliczenia: <strong>0</strong> </p>
+      <p>
+        <button className="form__button">Oblicz</button>
+      </p>
+      <p className="form__additionalInformation">*Pole obowiąznowe do wypełnienia</p>
+      <p className="form__additionalInformation">Obliczenia wykonano dla kursu walut z dnia 3.11.2022.</p>
+    </fieldset>
+  </form>
 );
 
 export default Form;
