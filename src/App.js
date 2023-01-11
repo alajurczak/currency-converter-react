a3249671fcb3582676aba66756450c3765b53f80
Alicja Jurczak <ala.jurczak@gmail.com>
2023-01-11 19:29:07 +0100

    error message added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,50 @@ import { useState, useEffect } from "react";
 function App() {
   const [rates, setRates] = useState({});
   const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  useEffect(() => {
+  const fetchApi = () => {
     fetch("https://api.exchangerate.host/latest?base=PLN")
+      .then((response) => {
+        if (response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response;
+      })
       .then((response) => response.json())
       .then((dataRates) => {
         setRates(dataRates.rates);
         setDate(dataRates.date);
-      });
-    console.log(rates);
+        setLoading(false);
+      })
+      .catch(() => setError(true));
+  };
+  useEffect(() => {
+    setTimeout(fetchApi, 3000);
   }, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <p>
+          Ładowanie... Pobieram aktualne kursy z Banku Centralnego.
+        </p>
+        <CurrentDate />
+      </Container>
+    )
+  }
+
+  if (error) {
+    return (
+      <Container>
+        <p>
+          Upss... Coś poszło nie tak! Sprawdź połączenie z internetem i spróbuj ponownie.
+        </p>
+        <CurrentDate />
+      </Container>
+    )
+  }
 
   return (
     <Container>
