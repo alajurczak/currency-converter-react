1d26ff6bfc61fa31275421ddb12bd21cbf895b54
Alicja Jurczak <ala.jurczak@gmail.com>
2023-01-13 17:51:33 +0100

    useCurrentRatesData hook added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import Form from "./Form";
 import Container from "./Container";
 import CurrentDate from "./Date";
-import { useState, useEffect } from "react";
 import { StyledParagraph } from "./Container/styled";
 import { Fieldset, Legend } from "./Form/styled";
+import { useCurrentRatesData } from "./useCurrentRatesData";
 
 const LoadingText = "Pobieram aktualne kursy z Narodowego Banku Polskiego...";
 const ErrorText = "Upss... Coś poszło nie tak! Sprawdź połączenie z internetem i spróbuj ponownie.";
 
 function App() {
-  const [rates, setRates] = useState({});
-  const [date, setDate] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const fetchApi = async () => {
-    try {
-      const myResponse = await fetch("https://api.exchangerate.host/latest?base=PLN");
-      if (!myResponse.ok) {
-        throw new Error(myResponse.statusText);
-      }
-      const dataRates = await myResponse.json();
-      setRates(dataRates.rates);
-      setDate(dataRates.date);
-      setLoading(false);
-    }
-    catch (error) {
-      setLoading(false);
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    setTimeout(fetchApi, 1000);
-  }, []);
+  const { rates, date, loading, error } = useCurrentRatesData();
 
   const isReady = !error && !loading;
 
