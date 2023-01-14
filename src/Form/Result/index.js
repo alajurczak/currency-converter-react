dd38a3dba1b4ab3fd4074752f79f1340e891af33
Alicja Jurczak <ala.jurczak@gmail.com>
2023-01-14 16:13:12 +0100

    early return used

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,7 +1,10 @@
 import { StyledResult } from "./styled";
 
 export const Result = ({ result }) => {
-    if (result !== "") return (
+    if (!result) {
+        return null;
+    }
+    return (
         <StyledResult>
             Wynik przeliczenia:
             <strong> {result.sourceAmount} PLN = {(result.targetAmount).toFixed(2)} {result.currency}</strong>
